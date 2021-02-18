f00f011157092df83ecd96d664dfd87c78021ff2
Mathew Cook <mat.cook@bigpond.com>
2021-02-18 15:40:37 +0800

    added html-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/assets/scripts/main.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
   devtool: 'source-map',
 };
