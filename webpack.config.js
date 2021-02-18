7c4459c7ddf83bc164dd7bfc924fa3defe76f806
Mathew Cook <mat.cook@bigpond.com>
2021-02-18 14:44:19 +0800

    enabled hot reloading for html files

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, 'app'),
   },
   devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html');
+    },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
     port: 3000,
